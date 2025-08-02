339a785f62bf430a9dbb67168c8c528096d8860b
sadjedlou <s_louahouah@estin.dz>
2025-08-02 11:18:13 +0100

    add the models to the admin and continue with the blog and creatinng the user app

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'first content of blog post 1  ',
-        'date_posted': 'January 1, 2023'
-    },
-    {
-        'author': 'Jane Smith',
-        'title': 'Blog Post 2',    
-        'content': 'Content of blog post 2',
-        'date_posted': 'January 2, 2023'
-    },
     
-]
+
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()  # Fetching all Post objects from the database
     }
     return render(request, 'blog/home.html', context) 
 
