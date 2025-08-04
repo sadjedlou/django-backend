f46dfa1af0c98de378efea7f803c8f5c23dda196
sadjedlou <s_louahouah@estin.dz>
2025-08-04 15:14:27 +0100

    start the login and the logout routed and the created the  login.html file

--- offdjangotuto/urls.py
+++ offdjangotuto/urls.py
@@ -17,11 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from users import views as user_views  # Import user views 
+from users import views as user_views  
+from django.contrib.auth import views as auth_views  
+# Import user views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),    
+    path('register/', user_views.register, name='register'),
+     path('login/', auth_views.LoginView.as_view(template_name ='users/login.html') , name='login'),    
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html') , name='logout'),     
     path('blog/', include('blog.urls')), 
      # Include the blog app's URLs
 ]
